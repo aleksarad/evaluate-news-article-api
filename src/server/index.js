941f2cfe1b9f1ed7945c5775a414bacb5b04d616
Aleksa <alexa.radevich1@gmail.com>
2020-01-06 16:42:07 -0500

    Initial commit

---
+++ src/server/index.js
@@ -0,0 +1,35 @@
+var path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const mockAPIResponse = require('./mockAPI.js');
+var aylien = require("aylien_textapi");
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+    });
+
+
+
+
+const app = express()
+
+app.use(express.static('dist'))
+
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+    console.log(`Your API key is ${process.env.API_KEY}`);
+
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
