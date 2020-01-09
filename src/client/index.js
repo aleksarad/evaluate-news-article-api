5938ffff3a03c3fd8bdd173af35aec82a9163baf
Aleksa <alexa.radevich1@gmail.com>
2020-01-09 13:50:55 -0500

    styling

--- src/client/index.js
+++ src/client/index.js
@@ -7,10 +7,12 @@ export {
 }
 
 import './styles/resets.scss'
-import './styles/base.scss'
-import './styles/footer.scss'
-import './styles/form.scss'
 import './styles/header.scss'
+import './styles/base.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+
+
 
 
 
