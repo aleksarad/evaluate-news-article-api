c0f70c11e1116b9fd807c60981cc3c367fc8e59a
Aleksa <alexa.radevich1@gmail.com>
2020-01-10 22:57:56 -0500

    service worker update

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,19 +1,22 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
     output: {
-    filename: 'bundle.min.js',
-        path: path.resolve(__dirname, './dist'),
-        libraryTarget:  'var',
+        libraryTarget: 'var',
         library: 'Client'
-},
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     module: {
         rules: [
             {
@@ -22,26 +25,17 @@ module.exports = {
                 loader: "babel-loader"
             },
             {
-		        test: /\.scss$/,
-		        use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
     },
     plugins: [
-        new CleanWebpackPlugin({
-            // Simulate the removal of files
-            dry: true,
-            // Write Logs to Console
-            verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
-            cleanStaleWebpackAssets: true,
-            protectWebpackAssets: false
-        }),
-        new WorkboxPlugin.GenerateSW(),
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new MiniCssExtractPlugin(),
+        new MiniCssExtractPlugin({filename: '[name].css'}),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
